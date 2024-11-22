c1c786f6c2605114d0afecd856deafa74ead8680
Tooyyib <adebisitooyyib@gmail.com>
2024-11-22 21:50:19 +0100

    changed from index to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 
-import Home from './pages/home/Home'
+import Home from "./pages/home/Home"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
