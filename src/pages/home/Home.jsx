80236c41a054f5047fbcf876f610d25783931d70
Tooyyib <adebisitooyyib@gmail.com>
2025-08-13 22:49:01 +0100

    updated images and added new contents

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,18 +3,17 @@ import chicken from "../../assets/chicken.png"
 import pepper from "../../assets/pepper.png"
 import tomat from "../../assets/tomat.png"
 import chicken2 from "../../assets/chicken2.png"
-import jolly1 from "../../assets/jolly1.jpg"
 import Button from "../../component/shared/button/Button"
 import Modals from "../../component/shared/modals/Modals"
 
-import egusi from "../../assets/egusi.jpeg"
-import fried from "../../assets/fried.jpeg"
+import elegusi from "../../assets/elegusi.jpg"
+import friedd from "../../assets/friedd.jpg"
 import frozen from "../../assets/frozen.jpeg"
-import grill from "../../assets/grill.jpeg"
-import pepperf from "../../assets/pepperf.jpeg"
+import grillis from "../../assets/grillis.jpg"
+import pepperf from "../../assets/pepperf.jpg"
 import foodcustom from "../../assets/foodcustom.webp"
 import custom from "../../assets/custom.jpg"
-import delivery from "../../assets/delivery.gif"
+import deliveryavater from "../../assets/deliveryavater.jpg"
 
 import React from 'react';
 import Typed from 'typed.js';
@@ -67,88 +66,100 @@ const Home = () => {
 
 
          </div>
-
-         {/* section 2 */}
       </div>
-      <div className="flex justify-evenly items-center bg-[#696d8b] h-screen text-white">
+
+{/* top selling packages */}
+
+      <div className="flex justify-evenly items-center bg-green-700 h-screen text-white">
 
         <div className="lg:w-[50%] flex flex-col gap-5 md:pl-5">
           <div className="text-5xl font-semibold drop-shadow-lg px-5">
           Our top selling packages
           </div>
-          <div className="text-2xl px-5">
+          <div className="text-2xl sm:hidden lg:block md:block
+           px-5">
           See what our beloving customers loved most this week, join our never ending dish - ORDER NOW!
           </div>
         </div>
-        <div className=""><img src={jolly1} className="w-[32rem] lg:inline rounded-[5rem] md:inline  hidden" alt="jolly1" /></div>
+        <div className=""><img src={custom} className="w-[32rem] lg:inline rounded-[5rem] md:inline  hidden" alt="jolly1" /></div>
       </div>
+
       {/* Food price list  */}
-      {/* section 3 */}
+    
       <div className="flex justify-center mt-[3rem]">
         <div id="menu" className="lg:grid lg:grid-cols-3 gap-10 space-y-2 md:grid md:grid-cols-2 lg:block">
           <div><Modals className='w-[20rem] h-[18rem] rounded-[10rem] drop-shadow-lg' text="Jollof Rice and Chicken" price="N1,500"  image={foodcustom}/></div>
-          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Frozen Chicken" price="N8,000"  image={frozen}/></div>
-          <div><Modals className='w-[18em]  h-[18rem] rounded-[50%] drop-shadow-lg' text="Fried Chicken" price="Chicken price + N2,500"  image={fried}/></div>
-          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Grill Chicken" price="Chicken price + N2,000"  image={grill}/></div>
-          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Pepper Fried/Soup Chicken" price="Chicken price + N3,500"  image={pepperf}/></div>
-          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Efo/Egusi Chicken" price="Chicken Price + N4,000" image={egusi}/></div>
+          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Frozen Chicken" price="N8-10,000"  image={frozen}/></div>
+          <div><Modals className='w-[18em]  h-[18rem] rounded-[50%] drop-shadow-lg' text="Fried Chicken" price="N10,500"  image={friedd}/></div>
+          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Grill Chicken" price="N10,000"  image={grillis}/></div>
+          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Pepper Fried/Soup Chicken" price="N11,500"  image={pepperf}/></div>
+          <div><Modals className='w-[18rem] h-[18rem] rounded-[50%] drop-shadow-lg' text="Efo/Egusi Chicken" price="N12,000" image={elegusi}/></div>
         </div>
       </div>
      
 
      <div className="flex  justify-center gap-5 items-center h-screen">
       <div className="md:pl-5">
-        <img className="rounded-[50%] lg:w-[30rem] md:block hidden" src={custom} alt="custom" />
+        <img className="rounded-[50%] lg:w-[30rem]" src={custom} alt="custom" />
       </div>
       <div className="flex flex-col gap-4">
         <div className="text-5xl w-[25rem] font-bold text-[#8fd521] px-3">Our favorite custom packages this week!</div>
-        <div className="mytext text-2xl w-[20rem] px-3">
+        <div className="mytext text-2xl w-[20rem] sm: hidden lg:block md:block px-3">
           We love it when you create your own custom 
           packages and express your creativity!
           See our favourite custom orders this week
         </div>
+
+        <img className="rounded-[50%] lg:w-[30rem] sm:hidden pr-[1rem]" src={custom} alt="custom" />
+
       </div>
      </div>
      
-    
-<div className="lg:flex justify-center items-center gap-10  my-[3rem] pl-3 pr-3 md:pl-5 md:text-[1.3rem]">
+    {/* event and corporate */}
+<div className="lg:flex justify-center gap-10  my-[3rem] pl-3 pr-3 md:pl-5 md:text-[1.3rem]">
+  {/* event */}
   <div className="flex flex-col gap-3">
-    <div className="flex floor text-3xl lg:w-[35rem] h-[10rem] md:w-[47rem] font-bold text-[#8ed520] rounded-lg pt-5 pl-7">
-      Event
+    <a href="http://wa.me/2348035791134">
+      <div  className="flex floor text-3xl lg:w-[35rem] h-[10rem] md:w-[47rem] font-bold text-[#ffff] rounded-lg pt-5 pl-7">
+    Event
     </div>
-    <div className="w-[30rem]">
+    </a>
+    
+    <div className="lg:w-[25rem] md:w-[30rem] text-1xl">
     An event without Mumtiah Foods is definitely just a gathering. It gets better; you can customise your order however you deem fit.
-    </div>
-    <div className="">
-     <a className="underline text-2xl hover:text-yellow-500" href="http://wa.me/2349037938452">ORDER FOR YOUR EVENT</a> 
     </div>
   </div>
 
+{/* corporate */}
   <div className="flex flex-col gap-3">
-    <div className="flex floors text-3xl lg:w-[35rem] h-[10rem] md:w-[47rem] font-bold text-[#ffff] rounded-lg pt-5 pl-7">
+    <a href="http://wa.me/2348035791134">
+      <div  className="flex floors text-3xl lg:w-[35rem] h-[10rem] md:w-[47rem] font-bold text-[#ffff] rounded-lg pt-5 pl-7">
     Corporate
     </div>
+    </a>
+    
     <div className="lg:w-[25rem] md:w-[30rem] text-1xl">
     Sometimes all you need for culture fit is a corporate hangout/event with a lot Mumtiah Foods. This is what we go to work for.
-    </div>
-    <div className="">
-      <a className="underline text-2xl hover:text-yellow-500" href="http://wa.me/2349037938452">PLACE ORDER</a>
     </div>
   </div>
 </div>
 
 {/* delivery  message */}
 
-<div className="flex justify-center gap-10 items-center bg-white mb-[2rem] md:px-5">
-      <div className="">
-        <img className=" rounded-lg lg:w-[30rem] sm:block" src={delivery} alt="custom" />
+
+<div className="flex  justify-center gap-5 items-center h-screen">
+      <div className="md:pl-5">
+        <img className="rounded-[50%] lg:w-[30rem]" src={deliveryavater} alt="custom" />
       </div>
-      <div className="space-y-5">
-        <div  className="text-3xl w-[25rem] font-bold text-[#8fd521]">Delivered to your door step</div>
-        <div className="text-2xl w-[27rem]">
-         The level of care and customer service our pilots provide is a testament to the filtering,
+      <div className="flex flex-col gap-4">
+        <div className="text-5xl w-[23rem] font-bold text-[#8fd521] px-3">Deliver to your door step</div>
+        <div className="text-2xl w-[20rem] sm: hidden lg:block md:block px-3">
+          The level of care and customer service our pilots provide is a testament to the filtering,
           training, and values we've instilled into them from the beginning.
         </div>
+
+        <img className="rounded-[50%] lg:w-[30rem] sm:hidden pr-[1rem]" src={deliveryavater} alt="custom" />
+
       </div>
      </div>
     </Container>
